045bc9ceeae3a8dc29cca0922bb3f9588ef7ee08
scherbak <scherbak.pavel5@yandex.ru>
2023-04-10 17:13:46 +0400

    Добавил config-server

--- microservice/src/main/java/ru/scherbak/microservice/controller/HelloWorldController.java
+++ microservice/src/main/java/ru/scherbak/microservice/controller/HelloWorldController.java
@@ -1,12 +1,20 @@
 package ru.scherbak.microservice.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HelloWorldController {
+    @Value("${text.version}")
+    String version;
+    @Value("${text.greeting}")
+    String greeting;
+
     @GetMapping("/test")
     public String test() {
-        return "Hello World!";
+        return version + " " + greeting;
     }
 }
